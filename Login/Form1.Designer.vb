83cfbccf11f91d7bb5c05a0e284abe368588a6d7
Rohan <Rohan@DESKTOP-1V0BMSK>
2017-03-21 00:46:18 -0700

    finished

--- Login/Form1.Designer.vb
+++ Login/Form1.Designer.vb
@@ -22,9 +22,126 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Dim EmailLabel As System.Windows.Forms.Label
+        Dim PasswordLabel As System.Windows.Forms.Label
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPW = New System.Windows.Forms.TextBox()
+        Me.MembersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Library_FINAL_FINALDataSet = New Login.Library_FINAL_FINALDataSet()
+        Me.MembersTableAdapter = New Login.Library_FINAL_FINALDataSetTableAdapters.MembersTableAdapter()
+        Me.TableAdapterManager = New Login.Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager()
+        Me.EmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.EmployeesTableAdapter = New Login.Library_FINAL_FINALDataSetTableAdapters.EmployeesTableAdapter()
+        EmailLabel = New System.Windows.Forms.Label()
+        PasswordLabel = New System.Windows.Forms.Label()
+        CType(Me.MembersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Library_FINAL_FINALDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'EmailLabel
+        '
+        EmailLabel.AutoSize = True
+        EmailLabel.Location = New System.Drawing.Point(40, 102)
+        EmailLabel.Name = "EmailLabel"
+        EmailLabel.Size = New System.Drawing.Size(35, 13)
+        EmailLabel.TabIndex = 15
+        EmailLabel.Text = "Email:"
+        '
+        'PasswordLabel
+        '
+        PasswordLabel.AutoSize = True
+        PasswordLabel.Location = New System.Drawing.Point(40, 125)
+        PasswordLabel.Name = "PasswordLabel"
+        PasswordLabel.Size = New System.Drawing.Size(56, 13)
+        PasswordLabel.TabIndex = 17
+        PasswordLabel.Text = "Password:"
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Location = New System.Drawing.Point(118, 170)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLogin.TabIndex = 19
+        Me.BtnLogin.Text = "Login"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(108, 99)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 20
+        '
+        'txtPW
+        '
+        Me.txtPW.Location = New System.Drawing.Point(108, 125)
+        Me.txtPW.Name = "txtPW"
+        Me.txtPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPW.Size = New System.Drawing.Size(100, 20)
+        Me.txtPW.TabIndex = 21
+        '
+        'MembersBindingSource
+        '
+        Me.MembersBindingSource.DataMember = "Members"
+        Me.MembersBindingSource.DataSource = Me.Library_FINAL_FINALDataSet
+        '
+        'Library_FINAL_FINALDataSet
+        '
+        Me.Library_FINAL_FINALDataSet.DataSetName = "Library_FINAL_FINALDataSet"
+        Me.Library_FINAL_FINALDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'MembersTableAdapter
+        '
+        Me.MembersTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CheckoutTableAdapter = Nothing
+        Me.TableAdapterManager.EmployeesTableAdapter = Nothing
+        Me.TableAdapterManager.MembersTableAdapter = Me.MembersTableAdapter
+        Me.TableAdapterManager.ResourcesTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Login.Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'EmployeesBindingSource
+        '
+        Me.EmployeesBindingSource.DataMember = "Employees"
+        Me.EmployeesBindingSource.DataSource = Me.Library_FINAL_FINALDataSet
+        '
+        'EmployeesTableAdapter
+        '
+        Me.EmployeesTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(297, 272)
+        Me.Controls.Add(Me.txtPW)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Controls.Add(EmailLabel)
+        Me.Controls.Add(PasswordLabel)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.MembersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Library_FINAL_FINALDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Library_FINAL_FINALDataSet As Library_FINAL_FINALDataSet
+    Friend WithEvents MembersBindingSource As BindingSource
+    Friend WithEvents MembersTableAdapter As Library_FINAL_FINALDataSetTableAdapters.MembersTableAdapter
+    Friend WithEvents TableAdapterManager As Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPW As TextBox
+    Friend WithEvents EmployeesBindingSource As BindingSource
+    Friend WithEvents EmployeesTableAdapter As Library_FINAL_FINALDataSetTableAdapters.EmployeesTableAdapter
 End Class
--- Login/Form1.vb
+++ Login/Form1.vb
@@ -1,3 +1,69 @@
 ﻿Public Class Form1
 
+
+
+    Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
+
+        Dim NumberOfRows As Integer
+        Dim RowData As Object
+        Dim Password As String = String.Empty
+        If txtUsername.Text = String.Empty Or txtPW.Text = String.Empty Then
+            MessageBox.Show("please enter your email and password")
+        Else
+            'how many records contain the email provided by the user?
+            NumberOfRows = MembersTableAdapter.FillByEmail(Library_FINAL_FINALDataSet.Members, txtUsername.Text)
+            'if there is one existing record with the email then get the user's record with the getdataby method
+            'and put the record (really point to the record) in the object "RowData" then get the email from the object.
+            'The dataset's index names will match the names of the columns in the table. 
+            'Thus RowData.Password points to the user's password
+            If NumberOfRows = 1 Then
+
+                RowData = MembersTableAdapter.GetDataByEmail(txtUsername.Text)(0)
+                Password = RowData.Password
+
+                If Password = txtPW.Text Then
+                    MessageBox.Show("Login successful")
+
+                    'Me.Hide()
+                Else
+                    MessageBox.Show("Login unsuccessful")
+                    txtPW.Clear()
+                End If
+
+            Else
+                If txtUsername.Text = String.Empty Or txtPW.Text = String.Empty Then
+                    MessageBox.Show("please enter your email and password")
+                Else
+                    'how many records contain the email provided by the user?
+                    NumberOfRows = EmployeesTableAdapter.FillByEmployeeEmail(Library_FINAL_FINALDataSet.Employees, txtUsername.Text)
+                    'if there is one existing record with the email then get the user's record with the getdataby method
+                    'and put the record (really point to the record) in the object "RowData" then get the email from the object.
+                    'The dataset's index names will match the names of the columns in the table. 
+                    'Thus RowData.Password points to the user's password
+                    If NumberOfRows = 1 Then
+
+                        RowData = EmployeesTableAdapter.GetDataByEmployeeEmail(txtUsername.Text)(0)
+                        Password = RowData.Password
+
+                        If Password = txtPW.Text Then
+                            MessageBox.Show("Login successful")
+
+                            'Me.Hide()
+                        Else
+                            MessageBox.Show("Login unsuccessful")
+                            txtPW.Clear()
+                        End If
+
+                    Else
+                        MessageBox.Show("No user has registered with this email, please try a different email or register as a new user")
+                    End If
+                End If
+            End If
+        End If
+
+    End Sub
+
+
 End Class
+
+
