e4d3f062b910442a51b4abeff92ef83699a4732d
Justin <Justin@JUSTINSCOMP>
2017-03-23 13:04:33 -0700

    form

---
+++ Login/Form2.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Title = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Author = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Genre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Checkout = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ReturnDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ReturnBTN = New System.Windows.Forms.Button()
+        Me.RenewBTN = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Title, Me.Author, Me.Genre, Me.Checkout, Me.ReturnDate})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 92)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(593, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Title
+        '
+        Me.Title.HeaderText = "Title"
+        Me.Title.Name = "Title"
+        '
+        'Author
+        '
+        Me.Author.HeaderText = "Author"
+        Me.Author.Name = "Author"
+        '
+        'Genre
+        '
+        Me.Genre.HeaderText = "Genre"
+        Me.Genre.Name = "Genre"
+        '
+        'Checkout
+        '
+        Me.Checkout.HeaderText = "Checkout Date"
+        Me.Checkout.Name = "Checkout"
+        '
+        'ReturnDate
+        '
+        Me.ReturnDate.HeaderText = "Return Date"
+        Me.ReturnDate.Name = "ReturnDate"
+        '
+        'ReturnBTN
+        '
+        Me.ReturnBTN.Location = New System.Drawing.Point(611, 125)
+        Me.ReturnBTN.Name = "ReturnBTN"
+        Me.ReturnBTN.Size = New System.Drawing.Size(75, 23)
+        Me.ReturnBTN.TabIndex = 1
+        Me.ReturnBTN.Text = "Return"
+        Me.ReturnBTN.UseVisualStyleBackColor = True
+        '
+        'RenewBTN
+        '
+        Me.RenewBTN.Location = New System.Drawing.Point(704, 125)
+        Me.RenewBTN.Name = "RenewBTN"
+        Me.RenewBTN.Size = New System.Drawing.Size(75, 23)
+        Me.RenewBTN.TabIndex = 2
+        Me.RenewBTN.Text = "Renew"
+        Me.RenewBTN.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 380)
+        Me.Controls.Add(Me.RenewBTN)
+        Me.Controls.Add(Me.ReturnBTN)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Title As DataGridViewTextBoxColumn
+    Friend WithEvents Author As DataGridViewTextBoxColumn
+    Friend WithEvents Genre As DataGridViewTextBoxColumn
+    Friend WithEvents Checkout As DataGridViewTextBoxColumn
+    Friend WithEvents ReturnDate As DataGridViewTextBoxColumn
+    Friend WithEvents ReturnBTN As Button
+    Friend WithEvents RenewBTN As Button
+End Class
